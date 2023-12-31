1bfeeb8a71f7e84079c026896bc2c639acc2ae0c
AliAzami97 <ali.azami97@gmail.com>
2023-12-31 18:11:14 +0330

    projects section from index page moved to partial view Projects

---
+++ Solution1/Project MVC/Models/Events.cs
@@ -0,0 +1,9 @@
+﻿namespace Project_MVC.Models
+{
+	public class Events
+	{
+		public long ID { get; set; }
+		public string title { get; set; }
+		public string Description { get; set; }
+	}
+}
---
+++ Solution1/Project MVC/Models/IndexModels.cs
@@ -0,0 +1,8 @@
+﻿namespace Project_MVC.Models
+{
+	public class IndexModel
+	{
+		public List<News> New { get; set; }
+		public List<Events> Event { get; set; }
+	}
+}
